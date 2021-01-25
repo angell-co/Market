abcddcd1a96879f90f3a8ecd5e95213fb2ced338
Josh Angell <josh@angell.io>
2021-01-25 16:52:10 +0000

    Order group record

---
+++ src/records/OrderGroup.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Market plugin for Craft Commerce
+ *
+ * A fully-stocked multi-vendor solution for Craft Commerce.
+ *
+ * @link      https://angell.io
+ * @copyright Copyright (c) 2020 Angell & Co
+ */
+
+namespace angellco\market\records;
+
+use angellco\market\db\Table;
+use craft\commerce\records\Customer;
+use craft\db\ActiveRecord;
+use yii\db\ActiveQueryInterface;
+
+/**
+ * Order group record
+ *
+ * @property int $id
+ * @property int $customerId
+ * @property string $email
+ * @property \DateTime $dateOrdered
+ * @property ActiveQueryInterface $customer
+ *
+ * @author    Angell & Co
+ * @package   Market
+ * @since     2.0.0
+ */
+class OrderGroup extends ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName(): string
+    {
+        return Table::ORDERGROUPS;
+    }
+
+    /**
+     * Returns the customer this destination belongs to.
+     *
+     * @return ActiveQueryInterface
+     */
+    public function getCustomer(): ActiveQueryInterface
+    {
+        return $this->hasOne(Customer::class, ['id' => 'customerId']);
+    }
+}
