cdee43a780cd6463e80dbaf05219212e47fce390
Josh Angell <josh@angell.io>
2021-03-17 12:32:07 +0000

    Categories finished off - scroll position and other bits

--- src/web/assets/vendordashboard/dist/js/categoryField.js
+++ src/web/assets/vendordashboard/dist/js/categoryField.js
@@ -1,12 +1,40 @@
+if (typeof Market === 'undefined') {
+    var Market = {
+        categoryFields: {}
+    };
+}
+
 function categoryField() {
     return {
         open: false,
+        fieldId: null,
 
-        refreshParent(fieldId, ids) {
-            htmx.find('#field-'+fieldId+'-selectedIds').value = ids;
-            htmx.trigger('#field-'+fieldId, 'refresh');
-            console.log(fieldId);
-            console.log(ids);
+        initField(fieldId) {
+            this.fieldId = fieldId;
+        },
+
+        onSave(ids) {
+            // Close the panel
+            this.open = false;
+
+            // Find the input in the parent, update it with the ids and then trigger a refresh
+            htmx.find('#field-'+this.fieldId+'-selectedIds').value = ids;
+            htmx.trigger('#field-'+this.fieldId, 'refresh');
+        },
+
+        trackScroll(evt) {
+            // Store the scroll of this field’s index
+            Market.categoryFields[this.fieldId] = {
+                scroll: htmx.find('#field-'+this.fieldId+'-categories-index-container').scrollTop
+            };
+        },
+
+        applyScroll(evt) {
+            // Check we have a scroll val stored and that the event is coming from
+            // the category toggle button and not anywhere else
+            if (typeof Market.categoryFields[this.fieldId] !== 'undefined' && evt.detail.requestConfig.elt.classList.contains("category-toggle")){
+                htmx.find('#field-'+this.fieldId+'-categories-index-container').scrollTop = Market.categoryFields[this.fieldId].scroll;
+            }
         }
     }
 }
